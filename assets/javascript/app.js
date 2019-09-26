7e93b58a3ce69be64625f3a8b8103289464a5d6e
Charlie Crocker <cscrocker84@gmail.com>
2019-09-26 17:37:56 -0500

    More math for the crystals, starting the game functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,4 +23,29 @@ function values(min, max) {
 };
 
 // generate random values for variables
+// create a random target number min 15 and max 115
+
+target = values(15, 115);
+
+// crystals values min = 1, max = 12    
+crystalone.value = values(1, 12);
+crystaltwo.value = values(1, 12);
+crystalthree.value = values(1, 12);
+crystalfour.value = values(1, 12);
+
+// Start the game, link it back to the html
+$(document).ready(function startgame() {
+    gamestart = true;
+    values();
+    $('#target').html("<p>Target: " + target + "</p>")
+    valuecheck();
+    console.log(target);
+    console.log(crystalOne);
+    console.log(crystalTwo);
+    console.log(crystalThree);
+    console.log(crystalFour);
+    console.log(gamestart);
+});
+
+
 
